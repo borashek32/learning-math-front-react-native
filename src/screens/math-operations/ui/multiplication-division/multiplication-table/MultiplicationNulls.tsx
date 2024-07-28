c3be1b438c1994df6aa327749dd989199c7e8aaf
Nataly Baranova <borashek@inbox.ru>
2024-07-28 18:08:50 +0300

    feat: set total user score functionality back

--- src/screens/math-operations/ui/multiplication-division/multiplication-table/MultiplicationNulls.tsx
+++ src/screens/math-operations/ui/multiplication-division/multiplication-table/MultiplicationNulls.tsx
@@ -20,7 +20,7 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import { Loader } from '../../../../../components/loaders/CircularLoader'
 import { Modal } from '../../../../../components/modal/Modal'
 import { Error } from '../../../../../components/error/Error'
-import { setTotalUserScore } from '../../../../profile/profile.slice'
+import { setTotalUserScore } from '../../../../../redux/slices/profile.slice'
 import { useDispatch } from 'react-redux'
 
 export const MultiplicationNulls = () => {
@@ -52,44 +52,44 @@ export const MultiplicationNulls = () => {
     setAnswer(answer)
   }
 
-  // const {
-  //   handleSubmit,
-  //   reset,
-  // } = useForm<ScoreType>({
-  //   defaultValues: {
-  //     score: score,
-  //     userId: useAppSelector(selectUserId), 
-  //     date: new Date()
-  //   },
-  //   mode: 'onChange',
-  //   resolver: yupResolver(formSchema) as Resolver<ScoreType>,
-  // })
+  const {
+    handleSubmit,
+    reset,
+  } = useForm<ScoreType>({
+    defaultValues: {
+      score: score,
+      userId: useAppSelector(selectUserId), 
+      date: new Date()
+    },
+    mode: 'onChange',
+    resolver: yupResolver(formSchema) as Resolver<ScoreType>,
+  })
 
-  // const onSubmit: SubmitHandler<ScoreType> = (data: ScoreType) => {
-  //   setServerError('')
-  //   const answerToNumber = Number(answer)
-  //   Keyboard.dismiss()
-  //   if (answerToNumber / firstMultiplier === secondMultiplier) {
-  //     setScore(score + 1)
-  //     setRightWrong('right')
-  //     data = { ...data, score: 1 }
-  //   } else {
-  //     setScore(score - 1)
-  //     setRightWrong('wrong')
-  //     data = { ...data, score: -1 }
-  //   }
+  const onSubmit: SubmitHandler<ScoreType> = (data: ScoreType) => {
+    setServerError('')
+    const answerToNumber = Number(answer)
+    Keyboard.dismiss()
+    if (answerToNumber / firstMultiplier === secondMultiplier) {
+      setScore(score + 1)
+      setRightWrong('right')
+      data = { ...data, score: 1 }
+    } else {
+      setScore(score - 1)
+      setRightWrong('wrong')
+      data = { ...data, score: -1 }
+    }
     
-  //   updateScore(data)
-  //     .unwrap()
-  //     .then(response => {
-  //       reset()
-  //       setOpen(true)
-  //       dispatch(setTotalUserScore(response.data.score))
-  //     })
-  //     .catch((e: any) => {
-  //       if (e.status === 'FETCH_ERROR') setServerError(t('errors.serverError'))
-  //     })
-  // }
+    updateScore(data)
+      .unwrap()
+      .then(response => {
+        reset()
+        setOpen(true)
+        dispatch(setTotalUserScore(response.data.score))
+      })
+      .catch((e: any) => {
+        if (e.status === 'FETCH_ERROR') setServerError(t('errors.serverError'))
+      })
+  }
 
   const check = () => {
     const answerToNumber = Number(answer)
@@ -157,8 +157,7 @@ export const MultiplicationNulls = () => {
             title={t('mathOperations.common.generate')}
           />
           <MathOperationButton
-            // buttonCallback={handleSubmit(onSubmit)}
-            buttonCallback={check}
+            buttonCallback={handleSubmit(onSubmit)}
             title={t('mathOperations.common.check')}
           />
         </ButtonsLayout>
